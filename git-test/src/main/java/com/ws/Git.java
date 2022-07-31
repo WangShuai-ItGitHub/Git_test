79283aa56e4bb26973dc4a2b3fd2a066fc1b80f2
ws <wangshuai12060112@163.com>
2022-07-31 18:54:18 +0800

    First commit

---
+++ git-test/src/main/java/com/ws/Git.java
@@ -0,0 +1,7 @@
+package com.ws;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
---
+++ git-test/src/main/java/com/ws/Git0.java
@@ -0,0 +1,7 @@
+package com.ws;
+
+public class Git0 {
+    public static void main(String[] args) {
+        System.out.println("022");
+    }
+}
